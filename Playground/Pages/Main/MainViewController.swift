6fbf9aa9bd233285ac728213df8dfb5d0ebf7856
Tugberk Masat <tugberkmasat@yahoo.com>
2023-05-29 01:48:27 +0300

    Add ScrollViewViewController and its empty storyboard

---
+++ Playground/Pages/Main/MainViewController.swift
@@ -0,0 +1,31 @@
+//
+//  MainViewController.swift
+//  Playground
+//
+//  Created by Çağrı Tuğberk Masat on 29.05.2023.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func scrollViewButtonTapped(_ sender: Any) {
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
