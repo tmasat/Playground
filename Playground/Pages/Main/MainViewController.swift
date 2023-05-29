8e23af43e7ad0e35aacc96d5ca395f4ee96cfeca
Tugberk Masat <tugberkmasat@yahoo.com>
2023-05-29 11:35:25 +0300

    Add vertical scroll view

--- Playground/Pages/Main/MainViewController.swift
+++ Playground/Pages/Main/MainViewController.swift
@@ -8,24 +8,18 @@
 import UIKit
 
 class MainViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-
+    
     @IBAction func scrollViewButtonTapped(_ sender: Any) {
+        guard let vc = self.storyboard?.instantiateViewController(withIdentifier: "ScrollViewViewController") as? ScrollViewViewController else { return }
+        
+        navigationController?.pushViewController(vc, animated: true)
+        
+     
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
